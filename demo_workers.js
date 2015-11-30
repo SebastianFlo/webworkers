978c90d8bca4c3186531049483631bde3b7272e1
Sebastian Florian <rebardesigndk@gmail.com>
2015-11-30 23:32:52 +0100

    feature (workers) send message to woker

--- demo_workers.js
+++ demo_workers.js
@@ -8,3 +8,10 @@ function timedCount() {
 }
 
 timedCount();
+
+onmessage = function(e) {
+  console.log('Message received from main script');
+  var workerResult = 'Result: ' + (e.data);
+  console.log('Posting message back to main script');
+  postMessage(workerResult);
+}
--- main.js
+++ main.js
@@ -35,3 +35,23 @@ function stopWorker() {
     //reuse web worker
     w = undefined;
 }
+
+function sendMessageToWorker(){
+    var id = document.getElementById("field").value;
+    //check browser support
+    if(typeof(Worker) !== "undefined") {
+        // create web worker object
+        if(typeof(w) == "undefined") {
+            w = new Worker("demo_workers.js");
+        }
+        //event sender
+        w.postMessage(id);
+        console.log('Message posted to worker');
+        
+        w.onmessage = function(event) {
+            document.getElementById("result").innerHTML = event.data;
+            console.log('Message received from worker');
+        }
+    }
+}
+
